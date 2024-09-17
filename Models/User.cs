1bcb27dec394c87cf1b6f1e0ad3c9e6635d750a9
lehuynhtuong <140950852+lehuynhtuong@users.noreply.github.com>
2024-09-17 23:00:04 +0700

    Create User.cs

---
+++ Models/User.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using BackEnd.Models;
+
+namespace BackEnd.Models;
+
+[Table("User")]
+public partial class User
+{
+    public long id { get; set; }
+
+    public string? email { get; set; }
+
+    public string? full_name { get; set; }
+
+    public string? password { get; set; }
+
+    public string? role { get; set; }
+}
